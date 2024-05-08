26d78cd504b809670af25eefea0db2453eecc6ab
fangzq <fangzq@bwoil.com>
2024-05-08 13:56:34 +0800

    fix: 打包优化

--- app-wenquan-console/craco.config.js
+++ app-wenquan-console/craco.config.js
@@ -1,7 +1,13 @@
+const WebpackBar = require('webpackbar');
+const { addBeforeLoaders, loaderByName } = require("@craco/craco");
 module.exports = {
   webpack: {
     configure(webpackConfig) {
+      addBeforeLoaders(webpackConfig, loaderByName("style-loader"), "thread-loader");
+      addBeforeLoaders(webpackConfig, loaderByName("style-loader"), "cache-loader");
       if (webpackConfig.mode === 'production') {
+        // 去除map文件
+         webpackConfig.devtool = false
         // 抽离公共代码，只在生产环境
         if (webpackConfig.optimization == null) {
           webpackConfig.optimization = {}
@@ -29,6 +35,9 @@ module.exports = {
       }
       return webpackConfig
     },
+    plugins: [
+      new WebpackBar()
+    ]
   },
 
   devServer: {
